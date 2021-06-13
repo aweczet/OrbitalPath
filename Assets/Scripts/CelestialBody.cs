e48eb594e840c6040a81b8e027cd6486e7f88d9d
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-13 10:46:30 +0200

    add: texture controll

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Celestial Body", menuName = "CelestialBody")]
+public class CelestialBody : ScriptableObject
+{
+    // https://upload.wikimedia.org/wikipedia/commons/6/64/Solar-System.pdf
+    public float distance;
+    public float radius;
+    public Texture texture;
+
+    // https://en.wikipedia.org/wiki/Orbital_period
+    public float orbitPeriod;
+
+    public float GetDistance(float scale)
+    {
+        return distance / scale;
+    }
+
+    public float GetRadius(float scale)
+    {
+        return radius / scale;
+    }
+}
--- Assets/Scripts/OrbitMotion.cs
+++ Assets/Scripts/OrbitMotion.cs
@@ -1,44 +1,62 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class OrbitMotion : MonoBehaviour
 {
-    public Transform orbitingObject;
-    public Ellipse orbitPath;
-
-    public float orbitProgress = 0f;
-    public float orbitPeriod = 3f;
-    public bool orbitActive = true;
+    public CelestialBody celestialBody;
+    
+    private Transform _orbitingObject;
+    private Ellipse _orbitPath;
+    
+    private float _orbitProgress;
+    private float _orbitPeriod;
+    private bool _orbitActive = true;
+    
+    private float _distanceScale = 10000000;
+    private float _radiusScale = 10000;
 
     private void Start()
     {
-        if (orbitingObject == null)
+        _orbitingObject = transform.GetChild(0);
+        if (_orbitingObject == null)
         {
-            orbitActive = false;
+            _orbitActive = false;
             return;
         }
+        _orbitPeriod = celestialBody.orbitPeriod * 5;
+        float distance = celestialBody.GetDistance(_distanceScale);
+        _orbitPath = new Ellipse(distance, distance);
+
+        float radius = celestialBody.GetRadius(_radiusScale);
+        _orbitingObject.localScale = new Vector3(radius, radius, radius);
+        
+        _orbitingObject.GetComponent<Renderer>().material.SetTexture("_MainTex", celestialBody.texture);
+
         SetOrbitingObjectPosition();
         StartCoroutine(StartOrbiting());
     }
 
     private void SetOrbitingObjectPosition()
     {
-        Vector2 orbitPosition = orbitPath.Evaluate(orbitProgress);
-        orbitingObject.localPosition = new Vector3(orbitPosition.x + transform.parent.GetChild(0).localPosition.x, 0, orbitPosition.y + transform.parent.GetChild(0).localPosition.z);
+        Vector2 orbitPosition = _orbitPath.Evaluate(_orbitProgress);
+        Vector3 parentPosition;
+        if ((parentPosition = transform.parent.GetChild(0).localPosition) == null)
+        {
+            parentPosition = transform.position;
+        }
+        _orbitingObject.localPosition = new Vector3(orbitPosition.x + parentPosition.x, 0, orbitPosition.y + parentPosition.z);
     }
 
     private IEnumerator StartOrbiting()
     {
-        if (orbitPeriod < .1f)
-            orbitPeriod = .1f;
+        if (_orbitPeriod < .1f)
+            _orbitPeriod = .1f;
 
-        float orbitSpeed = 1f / orbitPeriod;
-        while (orbitActive)
+        float orbitSpeed = 1f / _orbitPeriod;
+        while (_orbitActive)
         {
-            orbitProgress += Time.deltaTime * orbitSpeed;
-            orbitProgress %= 1f;
+            _orbitProgress += Time.deltaTime * orbitSpeed;
+            _orbitProgress %= 1f;
             SetOrbitingObjectPosition();
             yield return null;
         }
