18d8a29fec8d178f630fd3a0cf42eb9683929936
Albert Jendrzejczak <aweczet@gmail.com>
2021-06-15 11:34:19 +0200

    add: planet generator

---
+++ Assets/Scripts/CelestialBodiesGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Vector3 = UnityEngine.Vector3;
+
+public class CelestialBodiesGenerator : MonoBehaviour
+{
+    public Material[] materials;
+    
+    private int _celestialCount = 5000;
+    private float _maxRadius = 100f;
+    private List<GameObject> _celestialBodies;
+
+    private void Start()
+    {
+        _celestialBodies = GeneratreCelestialBodies();
+    }
+
+    private List<GameObject> GeneratreCelestialBodies()
+    {
+        var temporaryBodies = new List<GameObject>();
+        var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Rigidbody rigidbody = sphere.AddComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+
+        for (int i = 0; i < _celestialCount; i++)
+        {
+            var celestialBody = Instantiate(sphere);
+            celestialBody.transform.position = transform.position + new Vector3(
+                Random.Range(-_maxRadius, _maxRadius), Random.Range(-10f, 10f), Random.Range(-_maxRadius, _maxRadius));
+            celestialBody.transform.localScale *= Random.Range(.5f, 1f);
+
+            celestialBody.GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+            
+            temporaryBodies.Add(celestialBody);
+        }
+        
+        Destroy(sphere);
+
+        return temporaryBodies;
+    }
+
+    private void Update()
+    {
+        List<GameObject> celestialBodiesToRemove = new List<GameObject>();
+        foreach (GameObject celestialBody in _celestialBodies)
+        {
+            Vector3 difference = transform.position - celestialBody.transform.position;
+            float distance = difference.magnitude;
+
+            if (distance > 5 * _maxRadius)
+            {
+                celestialBodiesToRemove.Add(celestialBody);
+            }
+            
+            Vector3 gravityDirection = difference.normalized;
+            float gravity = 6.7f * (transform.localScale.x * celestialBody.transform.localScale.x * 80) / (distance * distance);
+            
+            Vector3 gravityVector = gravityDirection * gravity;
+            Rigidbody rigidbody = celestialBody.transform.GetComponent<Rigidbody>(); 
+            rigidbody.AddForce(celestialBody.transform.forward, ForceMode.Acceleration);
+            rigidbody.AddForce(gravityVector, ForceMode.Acceleration);
+        }
+
+        foreach (var body in celestialBodiesToRemove)
+        {
+            _celestialBodies.Remove(body);
+            Destroy(body);
+        }
+    }
+}
---
+++ Assets/Scripts/Ellipse/Controll.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controll : MonoBehaviour
+{
+    public GameObject[] cameras;
+    private int _cameraId = 0;
+
+    private TextMeshProUGUI _planetName;
+
+    private void Start()
+    {
+        foreach (var cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+        cameras[_cameraId].SetActive(true);
+        _planetName = transform.GetChild(1).GetChild(1).GetComponent<TextMeshProUGUI>();
+        ChangeName();
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Next()
+    {
+        cameras[_cameraId].SetActive(false);
+        _cameraId = _cameraId + 1 > cameras.Length - 1 ? 0 : _cameraId + 1;
+        cameras[_cameraId].SetActive(true);
+        ChangeName();
+    }
+
+    public void Back()
+    {
+        cameras[_cameraId].SetActive(false);
+        _cameraId = _cameraId - 1 < 0 ? cameras.Length - 1 : _cameraId - 1;
+        cameras[_cameraId].SetActive(true);
+        ChangeName();
+    }
+
+    public void ZoomOut()
+    {
+        Vector3 cameraPosition = cameras[_cameraId].transform.position;
+        float newCameraPosition = cameraPosition.y + 2 > 40 ? cameraPosition.y : cameraPosition.y + 2;
+        cameras[_cameraId].transform.position = new Vector3(cameraPosition.x, newCameraPosition, cameraPosition.z);
+    }
+
+    public void ZoomIn()
+    {
+        Vector3 cameraPosition = cameras[_cameraId].transform.position;
+        float newCameraPosition = cameraPosition.y - 2 < 2 ? cameraPosition.y : cameraPosition.y - 2;
+        cameras[_cameraId].transform.position = new Vector3(cameraPosition.x, newCameraPosition, cameraPosition.z);
+    }
+
+    private void ChangeName()
+    {
+        _planetName.text = cameras[_cameraId].transform.parent.parent.name;
+    }
+}
